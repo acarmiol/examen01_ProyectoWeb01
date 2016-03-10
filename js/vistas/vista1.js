2f662652a904d9d222b8e186e0e920708d823586
Adrian Carmiol <acarmiolu@ucenfotec.ac.cr>
2016-03-10 04:58:16 -0600

    updating forms and movements

--- js/vistas/vista1.js
+++ js/vistas/vista1.js
@@ -18,11 +18,29 @@ angular.module('myApp.vista1', ['ngRoute', 'firebase'])
 
 	}
 
-	$scope.showAddMovement = function(){
-		$scope.addMovementShow = true;
+	$scope.showEditForm = function(account){
+		$scope.editFormShow = true;
+
+		$scope.id = account.$id;
+
+		$scope.name = $scope.name;
+		$scope.tipo = $scope.tipo;
+		$scope.moneda = $scope.moneda;
+
+		$scope.movements.fecha = $scope.fecha;
+		$scope.movements.amount = $scope.amount;
+		$scope.movements.detail = $scope.detail;
+
 	}
 
+
+
+	// $scope.showAddMovement = function(){
+	// 	$scope.addMovementShow = true;
+	// }
+
 	$scope.hide = function(){
+		$scope.addFormShow = false;
 		$scope.accountShow = false;
 	}
 
@@ -32,13 +50,25 @@ angular.module('myApp.vista1', ['ngRoute', 'firebase'])
 		if($scope.tipo){ var tipo = $scope.tipo; } else { var tipo = null; }
 		if($scope.moneda){ var moneda = $scope.moneda; } else { var moneda = null; }
 
+		if($scope.fecha){ var fecha = $scope.fecha; } else { var fecha = null; }
+		if($scope.amount){ var amount = $scope.amount; } else { var amount = null; }
+		if($scope.detail){ var moneda = $scope.detail; } else { var detail = null; }
+
 		$scope.account.$add({
 			name : name,
 			tipo : tipo,
-			moneda : moneda
+			moneda : moneda,
+
+			movements : [{
+				fecha: fecha,
+				amount :amount,
+				detail: detail
+			}]
+			
+
 		}).then(function(ref){
 			var id = ref.key();
-			console.log('added contact with id' + id);
+			
 
 			clearFields();
 
@@ -49,6 +79,33 @@ angular.module('myApp.vista1', ['ngRoute', 'firebase'])
 
 	}
 
+	$scope.editFormSubmit = function(){
+
+
+		var id = $scope.id;
+
+		var record = $scope.accounts.$getRecord(id);
+
+		record.name = $scope.name;
+		record.tipo = $scope.tipo.singleSelect;
+		record.moneda = $scope.moneda.singleSelect;
+
+		record.movements[0].fecha= $scope.fecha.value;
+		record.movements[0].amount = $scope.amount;
+		record.movements[0].detail = $scope.detail;
+
+
+
+
+		$scope.accounts.$save(record).then(function(ref){
+			console.log(ref.key);
+		});
+		clearFields();
+
+		$scope.msg = "cuenta actualizada";
+	}
+
+
 	$scope.showAccount = function(account){
 		$scope.name = account.name;
 		$scope.tipo = account.tipo.singleSelect;
@@ -57,16 +114,21 @@ angular.module('myApp.vista1', ['ngRoute', 'firebase'])
 		$scope.accountShow = true;
 	}
 
+	$scope.removeAccount = function(accounts){
+		
+
+		$scope.account.$remove(accounts);
+
+		$scope.msg="Account Removed";
+	}
+
 	function clearFields(){
 		$scope.name = "";
 		$scope.tipo = "";
 		$scope.moneda = "";
 	}
 
-	$scope.addMovementSubmit = function(){
-		console.log('Adding Movement...');
-	}
-
+	
 
 
 }]);
